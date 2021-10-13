20bcc011e1ac795e65e3cf9dfc48d6efd2ecc977
ricardo564 <ricardo_hks@hotmail.com>
2021-10-13 12:32:03 -0300

    adding files
    adicionando arquivos

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <router-link :to="{ name: 'EventList'}">Events</router-link> |
+    <router-link :to="{ name: 'About'}">About</router-link>
+  </div>
+
+  <router-view />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#app {
+  padding: 30px;
+}
+
+#app a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#app a.router-link-exact-active {
+  color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
+}
+</style>
