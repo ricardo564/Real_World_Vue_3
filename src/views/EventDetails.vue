3214a1f6a54e5d7b5e8309e94627e799bbb64b04
Ricardo Camilo <ricardo_hks@hotmail.com>
2021-10-17 15:21:55 -0300

    Revert "Destalhes"
    This reverts commit 4eb17c5352c2ca94e0dd33967f46fccc1d2bf031.

--- src/views/EventDetails.vue
+++ src/views/EventDetails.vue
@@ -1,7 +1,7 @@
 <template>
     <div v-if="event.currentEvent">
         <h1>{{ event.currentEvent.title }}</h1>
-        <p> {{ event.currentEvent.time }} on {{ event.currentEvent.date }} 
+        <p> {{ event.currentEvnt.time }} on {{ event.currentEvent.date }} 
             @ {{ event.currentEvent.location }}</p>
         <p>{{event.currentEvent.description }}</p>
     </div>
