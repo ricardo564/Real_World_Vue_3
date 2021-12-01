5a007d1398bd5c1e345f6071d49a9eb9ed507e02
Ricardo Camilo <ricardo_hks@hotmail.com>
2021-11-30 23:19:51 -0300

    Programmatic Navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,20 @@
 <template>
   <p>Register for the event here.</p>
+  <button @click="register">Register Me</button>
 </template>
 
 <script>
 export default {
   props: ['id'],
+  methods: {
+    register() {
+      // Call to API //
+      // If registered then redirect to details //
+      this.$router.push({
+        name: 'EventDetails',
+        // params: { id: this.event.id }, //
+      })
+    },
+  },
 }
 </script>
